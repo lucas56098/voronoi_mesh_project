c6f96659cf96c124a545afa9dcf019225f633a11
lucas56098 <lucas@dr-schleuss.de>
2023-11-17 23:41:35 +0100

    remove local files from github

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/lucas_schleuss/vs code/voronoi_mesh_project
-# Build directory: /Users/lucas_schleuss/vs code/voronoi_mesh_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
